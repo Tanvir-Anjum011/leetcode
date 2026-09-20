b8f25a3263415fa92da9dc647b7339f0d902592a
Tanvir Anjum Turjo <tanvirturjo21@gmail.com>
2026-09-20 20:16:20 +0600

    136

---
+++ Array/136.Single Number.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  int singleNumber(vector<int> &nums)
+  {
+    int c = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+      c = c ^ nums[i];
+    }
+    return c;
+  }
+};
